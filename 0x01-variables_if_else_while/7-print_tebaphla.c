c550056219d2299631a66c731869ff7d01e3d140
RObiayo <ronnieobiayo59@gmail.com>
2023-02-17 16:38:44 -0800

    7-print_tebaphla.c

---
+++ 0x01-variables_if_else_while/7-print_tebaphla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints the lower case alphabets in reverse,
+ * followed by a new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
