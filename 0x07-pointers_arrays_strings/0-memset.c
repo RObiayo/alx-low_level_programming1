59eb45861eef460de80b2d88ef8e891afd711ac0
RObiayo <ronnieobiayo59@gmail.com>
2023-03-06 01:17:58 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+	s[i] = b;
+	n--;
+	}
+	return (s);
+}
