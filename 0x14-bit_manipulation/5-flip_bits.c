6a8b5646789cbfda82a6ca50ea1b8463ec1dc786
RObiayo <ronnieobiayo59@gmail.com>
2023-04-06 07:05:18 -0700

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,13 +8,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int diff - n ^ m;
 	unsigned int count = 0;
+	unsigned long int xor_value = n ^ m;
 
-	while (diff)
+	while (xor_value != 0)
 	{
-		count += diff & 1;
-		diff >>= 1;
+		count += xor_value & 1;
+		xor_value >>= 1;
 	}
 	return (count);
 }
